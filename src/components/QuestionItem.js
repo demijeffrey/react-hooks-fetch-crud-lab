cb96b2176b811e4c722b9ec9211206d2d74ea24f
Demaris Ashmore <demiashmore@yahoo.com>
2022-10-03 20:25:13 -0600

    commit tree 2f8aa85a60ea2aad19099f23a5c6a5b5c844342c

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function QuestionItem({ question }) {
+function QuestionItem({ question, handleDelete, handleChange }) {
   const { id, prompt, answers, correctIndex } = question;
 
   const options = answers.map((answer, index) => (
@@ -9,15 +9,16 @@ function QuestionItem({ question }) {
     </option>
   ));
 
+
   return (
     <li>
       <h4>Question {id}</h4>
       <h5>Prompt: {prompt}</h5>
       <label>
         Correct Answer:
-        <select defaultValue={correctIndex}>{options}</select>
+        <select onChange={(e) => handleChange(id, e.target.value)} defaultValue={correctIndex}>{options}</select>
       </label>
-      <button>Delete Question</button>
+      <button onClick={() => handleDelete(id)}>Delete Question</button>
     </li>
   );
 }
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,61 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import QuestionItem from './QuestionItem'
 
 function QuestionList() {
+  // console.log(questions)
+  
+  // function handleClick(e) {
+  //   handleDelete(e.target.id)
+  // }
+
+  // function onChange(e) {
+  //   const id = e.target.id
+  //   const index = e.target.value
+
+  //   handleChange(id, index)
+  // }
+
+  const [questions, setQuestions] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:4000/questions')
+      .then(res => res.json())
+      .then(data => setQuestions(data))
+  }, [questions])
+
+
+
+  function handleDelete(id) {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: 'DELETE'
+    })
+  }
+
+  function handleChange(id, index) {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify({
+        correctIndex: parseInt(index)
+      })
+    })
+    .then(res => res.json())
+    .then(updatedItem => console.log(updatedItem))
+  }
+
+  const prompts = questions.map(question => {
+    // console.log(question.answers[0])
+    return (
+      <QuestionItem question={question} key={question.id} handleChange={handleChange} handleDelete={handleDelete} />
+    )
+  })
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{prompts}</ul>
     </section>
   );
 }
